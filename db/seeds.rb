337eab0f954539ba94e121b644007b99db3e6864
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-02-01 15:03:42 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+pa = Artist.create!(name: "Paula Abdul")
+pa.songs.create!(title: "Straight Up")
+pa.songs.create!(title: "Opposites Attract")
+pa.songs.create!(title: "Cold Hearted")
+pa.songs.create!(title: "Forever Your Girl")
+
+madonna = Artist.create!(name: "Madonna")
+madonna.songs.create!(title: "Holiday")
+madonna.songs.create!(title: "Lucky Star")
+madonna.songs.create!(title: "Open Your Heart")
+madonna.songs.create!(title: "Material Girl")
+
